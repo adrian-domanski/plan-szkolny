ead181237438031a45e2987e1376c599a60b4a1e
Adrian <adrian.domanski.dev@gmail.com>
2021-08-06 12:55:14 +0200

    Api link changed

--- client/lib/withApollo.js
+++ client/lib/withApollo.js
@@ -1,22 +1,22 @@
-import withApollo from "next-with-apollo";
-import ApolloClient from "apollo-client";
-import { ApolloProvider } from "react-apollo";
-import { createUploadLink } from "apollo-upload-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import cookie from "next-cookies";
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { createUploadLink } from 'apollo-upload-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import cookie from 'next-cookies';
 
-const isServer = () => typeof window === "undefined";
+const isServer = () => typeof window === 'undefined';
 
 export default withApollo(
   ({ initialState, ctx }) => {
-    const dev = process.env.NODE_ENV !== "production";
+    const dev = process.env.NODE_ENV !== 'production';
     return new ApolloClient({
       cache: new InMemoryCache().restore(initialState || {}),
       link: createUploadLink({
-        credentials: "include",
+        credentials: 'include',
         uri: dev
-          ? "http://localhost:5000/graphql"
-          : "https://api.plan-szkolny.pl/graphql",
+          ? 'http://localhost:5000/graphql'
+          : 'https://api.plan-szkolny.kodario.pl/graphql',
         headers: isServer() && {
           token: cookie(ctx).token,
         },
--- server/server.js
+++ server/server.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const graphqlHTTP = require("express-graphql");
-const schema = require("./graphql/schema");
-const cors = require("cors");
-const { graphqlUploadExpress } = require("graphql-upload");
-const nodemailer = require("nodemailer");
-const config = require("config");
-const cookieParser = require("cookie-parser");
-const initChatbox = require("./helpers/chatbox");
-const buildLoaders = require("./graphql/loaders/buildLoaders");
-const { clearAccountsSchedule } = require("./helpers/database");
+const express = require('express');
+const mongoose = require('mongoose');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./graphql/schema');
+const cors = require('cors');
+const { graphqlUploadExpress } = require('graphql-upload');
+const nodemailer = require('nodemailer');
+const config = require('config');
+const cookieParser = require('cookie-parser');
+const initChatbox = require('./helpers/chatbox');
+const buildLoaders = require('./graphql/loaders/buildLoaders');
+const { clearAccountsSchedule } = require('./helpers/database');
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  service: 'Gmail',
   auth: {
-    user: config.get("gmailUser"),
-    pass: config.get("gmailPass"),
+    user: config.get('gmailUser'),
+    pass: config.get('gmailPass'),
   },
 });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Plan-Szkolny API");
+app.get('/', (req, res) => {
+  res.send('Plan-Szkolny API');
 });
 
 app.use(
   cors({
     credentials: true,
     origin: [
-      "http://localhost:3001",
-      "http://localhost:3000",
-      "https://www.plan-szkolny.pl",
-      "https://plan-szkolny.pl",
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'https://www.plan-szkolny.kodario.pl',
+      'https://plan-szkolny.kodario.pl',
     ],
   })
 );
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }),
   cookieParser(),
-  require("./middleware/auth"),
+  require('./middleware/auth'),
   graphqlHTTP((req) => {
     return {
       context: {
@@ -57,21 +57,19 @@ app.use(
 );
 
 // Confirm routes
-app.use("/confirm", require("./routes/confirm"));
+app.use('/confirm', require('./routes/confirm'));
 
-const PORT = process.env.NODE_ENV !== "production" ? 5000 : 4001;
+const PORT = process.env.NODE_ENV !== 'production' ? 5000 : 4001;
 
 mongoose.connect(
-  config.get("mongoURI"),
+  config.get('mongoURI'),
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   },
-  () => console.log("MongoDB Connected...")
+  () => console.log('MongoDB Connected...')
 );
-
-
 
 const server = app.listen(PORT, () =>
   console.log(`Server started on port: ${PORT}...`)
